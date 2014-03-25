73a5b69db0d4f8e183a0f3df74811e45aef5e1bd
vandosant <sjskender@gmail.com>
2014-03-25 07:46:06 -0600

    Task page removed
    Homepage will serve as place to view all tasks

--- app.rb
+++ app.rb
@@ -8,15 +8,9 @@ class App < Sinatra:: Base
     erb :index
   end
 
-  get "/:task" do
-    TaskDatabase.add(params[:task])
-    @tasks = TaskDatabase::TASKS
-    erb :task
-  end
-
   post "/" do
     TaskDatabase.add(params[:task])
     @tasks = TaskDatabase::TASKS
-    erb :task
+    erb :index
   end
 end
